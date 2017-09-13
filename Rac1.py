c7939534732206a72b27b80e39755c7d02253e4e
emibcn <emibcn@users.noreply.github.com>
2017-09-13 13:55:53 +0200

    Add license to source code script

--- Rac1.py
+++ Rac1.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+#    Rac1.py: listen to Rac1 catalan radio station from its public podcasts
+#
+#    Copyright (C) 2017  Emilio del Giorgio
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Python Dependencies:
 #  - httplib/http.client
