f967ee2690eff6f5938e8f895ff5cf0625aa9273
emibcn <emi@algorismia.com>
2018-10-20 13:32:28 +0200

    Fix #1 & Fix #3 - Do it pip installable & use `requests` instead of http/httplib

---
+++ bin/Rac1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from Rac1 import main
+
+if __name__ == "__main__":
+   exit(main())
---
+++ setup.py
@@ -0,0 +1,28 @@
+
+from setuptools import setup, find_packages
+
+VERSION = (1, 0, 3)
+__version__ = '.'.join(map(str, VERSION))
+
+setup(
+    name = 'Rac1',
+    version = __version__,
+    description = "Listen to Rac1 catalan radio station from its public podcasts",
+    long_description = "Small script to listen to Rac1 catalan radio station from its public podcasts at http://www.rac1.cat/a-la-carta",
+    author = "Emilio del Giorgio",
+    author_email = "https://github.com/emibcn",
+    url = "https://github.com/emibcn/Rac1.py",
+    license = "GPLv3",
+    platforms = ["any"],
+    packages = ['Rac1'],
+    py_modules=['Rac1.py'],
+    scripts = ["bin/Rac1.py"],
+    install_requires = ['requests', 'argparse', 'parsedatetime'],
+    include_package_data = True,
+    classifiers = [
+        "Environment :: Command line",
+        "License :: OSI Approved :: GPLv3 License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+)
